12021e837bfdb0eee3fcdd5d3a86d35dfbc018fd
trannamtb18032003 <131521035+trannamtb18032003@users.noreply.github.com>
2023-04-25 22:51:21 +0700

    Create database_taohadon.sql

---
+++ database_taohadon.sql
@@ -0,0 +1,12 @@
+CREATE TABLE HoaDonDatBan (
+  MaHoaDon INT PRIMARY KEY AUTO_INCREMENT,
+  NgayDat DATE,
+  TenKhachHang VARCHAR(50),
+  SoDienThoai VARCHAR(20),
+  Email VARCHAR(50),
+  SoLuongNguoi INT,
+  GhiChu VARCHAR(255),
+  TongTien INT
+);
+INSERT INTO HoaDonDatBan (NgayDat, TenKhachHang, SoDienThoai, Email, SoLuongNguoi, GhiChu, TongTien)
+VALUES ('2023-04-25', 'Nguyen Van A', '0123456789', 'nguyenvana@gmail.com', 4, 'Yeu cau phuc vu dac biet', 1000000);
