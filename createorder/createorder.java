5e8c29238ee40146de0228e633176e3c22186b43
Cokoroe <131521226+Cokoroe@users.noreply.github.com>
2023-04-26 15:09:17 +0700

    Add files via upload

---
+++ createorder/createorder.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class createorder {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField2;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JButton bankTransferButton;
+    private JTable thongkemonan;
+    private JTextField textField10;
+    private JButton updateButton;
+    private JButton returnButton;
+    private JButton cashButton;
+
+    public createorder() {
+    thongkemonan.addComponentListener(new ComponentAdapter() {
+    });
+}
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ payment/Payment.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Payment {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton bankTransferButton;
+    private JButton cashButton;
+    private JTextField textField6;
+    private JButton saveButton;
+    private JTextField textField7;
+}
