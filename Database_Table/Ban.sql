4986b5ca95f554230790fca7aa92b630113f0ed2
NTMQUAN <131521158+NTMQUAN@users.noreply.github.com>
2023-04-26 13:42:31 +0700

    Delete Database_Table directory

--- Database_Table/Ban.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO ban.ban (ban, trang_thai, ten_tiec, nguoi_lien_he, so_luong, ngay_dat, gio_dat)
-VALUES ('1', 'Đã đặt', 'Tiệc sinh nhật', 'Nguyễn Văn A', 10, '2023-05-01', '19:00:00');
-
-INSERT INTO ban.ban (ban, trang_thai, ten_tiec, nguoi_lien_he, so_luong, ngay_dat, gio_dat)
-VALUES ('2', 'Trống', '', '', 0, '', '');
-select * from ban.ban;
